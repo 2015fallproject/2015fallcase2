dea63b0d76691ddd14c130db85191390c6ce53fa
coursemdetw <course@mde.tw>
2015-10-27 11:47:08 +0800

    initial add

---
+++ config.py
@@ -0,0 +1,11 @@
+import authomatic
+from authomatic.providers import oauth2
+
+CONFIG = {
+        'google': {
+            'class_': oauth2.Google,
+            'consumer_key': '864853543778-vfdhe8hvr6ltb1bjisvmjmfr9372arai.apps.googleusercontent.com',
+            'consumer_secret': 'VSgv_v6kmtJHDPLUAJ5POhEi',
+            'scope': oauth2.Google.user_info_scope
+        }
+    }
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='KMOL 2015fall Case2',
+      version='1.0',
+      description='KMOL 2015fall Case2 OpenShift App',
+      author='KMOL',
+      author_email='2015fallproject@gmail.com',
+      url='https://www.python.org/community/sigs/current/distutils-sig',
+      install_requires=['Flask>=0.10.1', 'MarkupSafe', 'github3.py', 'authomatic'],
+     )
